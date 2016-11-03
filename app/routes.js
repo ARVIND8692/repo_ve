aaa33b6f3642e089759d990c79f64c55a55e439b
Arvind Shah <arvind8692@gmail.com>
2016-11-04 02:54:15 +0530

    this is plain simple login system

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 var Todo = require('./models/todo');
 
-module.exports = function(app) {
+module.exports = function(app,passport) {
 
 	// api ---------------------------------------------------------------------
 	// get all todos
@@ -56,7 +56,89 @@ module.exports = function(app) {
 	});
 
 	// application -------------------------------------------------------------
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	app.get('/home', function(req, res) {
+		res.sendfile('./assets/htmls/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
+
+	app.get('/lab/todo', function(req, res) {
+		res.sendfile('./assets/htmls/todo.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
+
+
+	    // =====================================
+    // HOME PAGE (with login links) ========
+    /*// =====================================
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
+*/
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.sendfile('./assets/htmls/login.html'); 
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.sendfile('./assets/htmls/signup.html');
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.sendfile('./assets/htmls/profile.html', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+	// route middleware to make sure a user is logged in
+	function isLoggedIn(req, res, next) {
+
+	    // if user is authenticated in the session, carry on 
+	    if (req.isAuthenticated())
+	        return next();
+
+	    // if they aren't redirect them to the home page
+	    res.redirect('/');
+	}
+
+	  // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+      // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 };
--- server.js
+++ server.js
@@ -9,18 +9,40 @@ var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+
 // configuration ===============================================================
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/assets')); 				// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+//app.set('view engine', 'ejs'); // set up ejs for templating
+
+ require('./config/passport')(passport); // pass passport for configuration
+
+
+app.use(cookieParser()); // read cookies (needed for auth)
+
+
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // routes ======================================================================
-require('./app/routes.js')(app);
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
